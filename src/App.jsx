08c274f700dcea1a5a5838656fc7269046a82d98
Lautaro Montaña <joselautarom@gmail.com>
2023-10-20 15:56:28 -0300

    Iniciamos módulo en react y primer componente

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import HolaMundo from './components/HolaMundo/HolaMundo';
+import {HolaMundo} from './components/HolaMundo/HolaMundo'
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <HolaMundo/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HolaMundo/HolaMundo.jsx
@@ -0,0 +1,21 @@
+//Forma número 2
+import React from 'react'
+
+export const HolaMundo = () => {
+  return (
+    <div>HolaMundo</div>
+  )
+}
+
+//Forma numero 1
+// import React from 'react'
+
+// const HolaMundo = () => {
+//     return(
+//       <>
+      
+//       </>
+//     )
+// }
+//
+//export default HolaMundo;
